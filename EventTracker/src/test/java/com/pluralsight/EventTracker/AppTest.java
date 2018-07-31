ee4a785bbe89144aa6ad85a0f07b708fe7c4e080
rohitoist <39763150+rohitoist@users.noreply.github.com>
2018-08-01 00:55:31 +0100

    Update AppTest.java

--- EventTracker/src/test/java/com/pluralsight/EventTracker/AppTest.java
+++ EventTracker/src/test/java/com/pluralsight/EventTracker/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App  hg fjfj.
+ * Unit test for simple App  hggdf fjfj.
  */
 public class AppTest 
     extends TestCase
